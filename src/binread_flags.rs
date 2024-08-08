72377332fc497d060e3a1882e1c2db371538e43f
Jan Starke <jan.starke@posteo.de>
2024-08-08 21:12:16 +0200

    ShellLinkHeader ist BinWrite now

--- src/binread_flags.rs
+++ src/binread_flags.rs
@@ -5,17 +5,37 @@ macro_rules! binread_flags {
 
             fn read_options<R: std::io::Read + std::io::Seek>(
                 reader: &mut R,
-                _endian: binrw::Endian,
+                endian: binrw::Endian,
                 _args: Self::Args<'_>,
             ) -> binrw::BinResult<Self> {
                 use binrw::BinReaderExt;
-                let raw: $repr = reader.read_le()?;
+                let raw: $repr = match endian {
+                    binrw::Endian::Big => reader.read_be()?,
+                    binrw::Endian::Little => reader.read_le()?,
+                };
+                
                 match Self::from_bits(raw) {
                     Some(res) => Ok(res),
                     None => Err(binrw::Error::AssertFail {
                         pos: reader.stream_position()?,
                         message: format!("unable to convert '0x{raw:x}' to {}", stringify!($type)),
                     }),
+                }
+            }
+        }
+
+        impl binrw::BinWrite for $type {
+            type Args<'a> = ();
+        
+            fn write_options<W: std::io::Write + std::io::Seek>(
+                &self,
+                writer: &mut W,
+                endian: binrw::Endian,
+                _args: Self::Args<'_>,
+            ) -> binrw::BinResult<()> {
+                match endian {
+                    binrw::Endian::Big => self.bits().write_be(writer),
+                    binrw::Endian::Little => self.bits().write_le(writer),
                 }
             }
         }
